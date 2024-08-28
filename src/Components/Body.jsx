dc17fdf951bff05dc8ee38cff4dfd3de189a3c98
SAHIL SINGH <107126552+HYPERxFURY24@users.noreply.github.com>
2024-08-29 00:03:28 +0530

    Enhanced Input textarea

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useRef, useEffect } from 'react';
 import {context} from '../App';
 import '../styles.css';
 import Loader from 'react-js-loader';
@@ -12,6 +12,19 @@ export default function Body()
     const [isEditing, setIsEditing] = useState(false);
     const {todoList, addItem, updateItem} = useContext(context);
     const todos = todoList.data;
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        if(textarea)
+        {
+            textarea.style.height = 'auto';
+            textarea.style.height = `${textarea.scrollHeight}px`;
+
+        }
+    }, [newTodo]);
+
+
 
     function handleChange(e)
     {
@@ -32,19 +45,31 @@ export default function Body()
     {
         setIsEditing(true);
         setNewTodo(data);
+        textareaRef.current.focus();
     }
 
     return (
         <>
-            <div className='relative text-center m-auto mt-6 bg-white box-border min-h-max max-w-96 border-2 outline-2 shadow-2xl rounded-md'>
-                <input className='absolute left-0 top-1 min-w-8 min-h-5' type='checkbox' name='completed' checked={newTodo.completed} onChange={handleChange}></input>
-                <input className='min-w-80 ml-6 mb-0.5 outline-transparent focus:border-b-2 border-black' type='text' name='todo' placeholder='Add an item' autoComplete='off' value={newTodo.todo} onChange={handleChange}></input>
+            <div className='flex justify-between items-center m-auto mt-6 bg-white box-border min-h-max max-w-96 border-2 outline-2 shadow-2xl rounded-md'>
+                <input className='min-w-8 h-5' type='checkbox' name='completed' checked={newTodo.completed} onChange={handleChange}></input>
+                
+                <textarea 
+                className='min-w-80 min-h-8 resize-none outline-transparent focus:border-b-2 border-black overflow-hidden   ' 
+                type='text' 
+                name='todo' 
+                placeholder='Add an item' 
+                autoComplete='off' 
+                value={newTodo.todo} 
+                rows={1}
+                onChange={handleChange} 
+                ref={textareaRef}/>
+                
                 {isEditing
                 ? <SendSharpIcon onClick={save}/> 
-                : <AddIcon className='max-w-9 top-1 absolute right-0' onClick={save}/> }
+                : <AddIcon className='h-5' onClick={save}/> }
             </div>
             {todoList.loading 
-            ? <div className='my-36'>
+            ? <div className='my-36'>   
                 <Loader type='spinner-circle' bgColor='#BFA181'/>
             </div>
             : todos.filter((ele) => !ele.isDeleted).map((ele) =><Card key={ele.id} data={ele} onEdit={edit}/>)}
